f46b56a5ec9f985b48fe1462d512bbb93892afe4
Tim <timmcmackenjr@gmail.com>
2020-09-15 16:50:42 -0500

    trying to parse getline

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv)
 	char *line_buf = NULL;
 	size_t line_buf_size = 0;
 	int line_size;
+	stack_t *head;
+	char *command;
 
 	/* First Check, only run program + file path */
 	if (argc != 2)
@@ -38,12 +40,33 @@ int main(int argc, char **argv)
 		line_count++;
 
 		/* Show the line details */
-		printf("line[%03d]: contents: %s", line_count, line_buf);
+		command = strtok(line_buf, " ");
+		printf("%s\n", command);
 
 		/* Get the next line */
-	        line_size = getline(&line_buf, &line_buf_size, fp);
+	    line_size = getline(&line_buf, &line_buf_size, fp);
 	}
 
 	fclose(fp);
 	return (0);
 }
+
+
+/**
+*
+*
+*
+*
+
+
+char *trim(char *line_buf)
+{
+	int i = 0, j = 0;
+	char new_string[];
+
+	while (line_buf[i] == ' ' || line_buf[i] =='\t')
+		i++;
+	for(;linebuff[i] != '\0'; i++, j++)
+		new_string[j] = line_buf[i]; 
+}
+*/
