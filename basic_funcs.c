edc823ff1502d77ea7c3e397b0ccd366c7a8f0cd
Tim <timmcmackenjr@gmail.com>
2020-09-17 17:20:11 -0500

    removed all the print statements and added clean up to the end of main

--- basic_funcs.c
+++ basic_funcs.c
@@ -6,32 +6,23 @@ void _push(stack_t **stack, unsigned int line_number)
     stack_t *new_node = malloc(sizeof(stack_t));
     (void) line_number;
 
-    printf("We've made it to basic funcs!!\n");
     if (new_node == NULL)
     {
-      printf("New Node is null??\n");
         fprintf(stderr, "Error: malloc failed\n");
         clean_up(info->fp, stack, info->buffer);
         exit(EXIT_FAILURE);
     }
-    printf("Assigning things to new node\n");
     new_node->next = NULL;
     new_node->n = info->num;
-    printf("Next and N are assigned\n");
     if (*stack == NULL)
-      {
-	new_node->prev = NULL;
-	printf("prev is assigned null\n");
-      }
+    	new_node->prev = NULL;
     else
     {
-      oldtop = *stack;
-      new_node->prev = oldtop;
-      oldtop->next = new_node;
+        oldtop = *stack;
+        new_node->prev = oldtop;
+        oldtop->next = new_node;
     }
-    printf("assigning deref stack to new node\n");
     *stack = new_node;
-    printf("Umm where is the problem?\n");
     return;
 }
 
--- cmd_comp.c
+++ cmd_comp.c
@@ -17,25 +17,18 @@ void cmd_comp(char *cmd[], stack_t **stack, unsigned int line_num)
 		{"pop", _pop}, {"add", _add}, {"sub", _sub}, {"div", _div},
 		{"mul", _mul}, {"nop", _nop}, {"mod", _mod},
 		{"swap", _swap}, */
-	printf("In cmd_comp after decs\n");
+
 	for (i = 0; opcodes[i].opcode != NULL; i++)
 	{
-	    printf("in the for loop to check op-codes\n");
 	    if (strcmp(opcodes[i].opcode, cmd[0]) == 0)
 		{
-		    printf("We have a match on %s\n", cmd[0]);
-		    printf("Next is %s\n", cmd[1]);
 			if (strcmp(opcodes[i].opcode, "push") == 0)
 			    {
-			        printf("The match is for push\n");
 			        /* I am going to need to check for spaces and fail conditions down the line */
 			        if (isdigit(cmd[1][0]) != 0)
 			        {
-				        printf("Inside the atoi if\n");
 				        info->num = atoi(cmd[1]);
-				        printf("After info->num assignment of %d\n", info->num);
 				        opcodes[i].f(stack, line_num);
-				        printf("We make it back to cmd_comp\n");
 				        return;
 			        }
 			        else
