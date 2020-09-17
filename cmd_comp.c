72e3bcea4d09517e261f80661e96cf5a1ccc117a
Tim <timmcmackenjr@gmail.com>
2020-09-17 17:00:15 -0500

    just some formatting clean up

--- cmd_comp.c
+++ cmd_comp.c
@@ -20,39 +20,38 @@ void cmd_comp(char *cmd[], stack_t **stack, unsigned int line_num)
 	printf("In cmd_comp after decs\n");
 	for (i = 0; opcodes[i].opcode != NULL; i++)
 	{
-	  printf("in the for loop to check op-codes\n");
-	  if (strcmp(opcodes[i].opcode, cmd[0]) == 0)
+	    printf("in the for loop to check op-codes\n");
+	    if (strcmp(opcodes[i].opcode, cmd[0]) == 0)
 		{
-		  printf("We have a match on %s\n", cmd[0]);
-		  printf("Next is %s\n", cmd[1]);
+		    printf("We have a match on %s\n", cmd[0]);
+		    printf("Next is %s\n", cmd[1]);
 			if (strcmp(opcodes[i].opcode, "push") == 0)
-			  {
-			    printf("The match is for push\n");
-			    /* I am going to need to check for spaces and fail conditions down the line */
-			    if (isdigit(cmd[1][0]) != 0)
-			      {
-				printf("Inside the atoi if\n");
-				info->num = atoi(cmd[1]);
-				printf("After info->num assignment of %d\n", info->num);
-				opcodes[i].f(stack, line_num);
-				printf("We make it back to cmd_comp\n");
-				return;
-			       }
-			    else
-			      {
-				fprintf(stderr, "L%u: usage: push integer\n", line_num);
-				clean_up(info->fp, stack, info->buffer);
-				exit(EXIT_FAILURE);
-			      }
-			  }
+			    {
+			        printf("The match is for push\n");
+			        /* I am going to need to check for spaces and fail conditions down the line */
+			        if (isdigit(cmd[1][0]) != 0)
+			        {
+				        printf("Inside the atoi if\n");
+				        info->num = atoi(cmd[1]);
+				        printf("After info->num assignment of %d\n", info->num);
+				        opcodes[i].f(stack, line_num);
+				        printf("We make it back to cmd_comp\n");
+				        return;
+			        }
+			        else
+			        {
+				        fprintf(stderr, "L%u: usage: push integer\n", line_num);
+				        clean_up(info->fp, stack, info->buffer);
+				        exit(EXIT_FAILURE);
+			        }
+			    }
 			else
-			  opcodes[i].f(stack, line_num);
+			    opcodes[i].f(stack, line_num);
 		}
 	}
 	if (opcodes[i].opcode == NULL)
-	  {
+	{
 	    fprintf(stderr, "L%u: unknown instruction %s\n", line_num, cmd[0]);
 	    clean_up(info->fp, stack, info->buffer);
-	  }
-    return;
+	}
 }
