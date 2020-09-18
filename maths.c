c1c20a9db19d9c26df207cddd6c475d839d2aeaa
Tim <timmcmackenjr@gmail.com>
2020-09-17 19:27:07 -0500

    Adds moving functions and math functions

--- maths.c
+++ maths.c
@@ -1,7 +1,86 @@
 #include "monty.h"
 
-void _add(stack_t **stack, unsigned int line_number);
-void _sub(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
-void _mul(stack_t **stack, unsigned int line_number);
-void _mod(stack_t **stack, unsigned int line_number);
+/**
+ * _add - adds top two nodes in stack
+ * @stack: pointer to top node in stack
+ * @line_number: line count for given file
+ */
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+    stack_t *mover = (*stack)->prev;
+	int result = 0;
+    (void) line_number;
+
+	result = ((*stack)->n + mover->n);
+	mover->n = result;
+	_pop(stack, line_number);
+}
+
+/**
+ * _sub - subtracts the second element n from the top element n
+ * @stack: pointer to top node in stack
+ * @line_number: line count for given file
+ */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *mover = (*stack)->prev;
+	int result = 0;
+	(void) line_number;
+
+	result = ((*stack)->n - mover->n);
+	mover->n = result;
+	_pop(stack, line_number);
+}
+
+/**
+ * _div - divids the second n by the top n.
+ * @stack: pointer to top node in stack
+ * @line_number: line count for given file
+ */
+
+void _div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *mover = (*stack)->prev;
+	int result = 0;
+	(void) line_number;
+
+	result = (mover->n / (*stack)->n);
+	mover->n = result;
+	_pop(stack, line_number);
+}
+
+/**
+ * _mul - multiplies the top two nodes in stack
+ * @stack: pointer to top node in stack
+ * @line_number: line count for given file
+ */
+
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *mover = (*stack)->prev;
+	int result = 0;
+	(void) line_number;
+
+	result = ((*stack)->n * mover->n);
+	mover->n = result;
+	_pop(stack, line_number);
+}
+
+/**
+ * _mod - second n modulus top n
+ * @stack: pointer to top node in stack
+ * @line_number: line count for given file
+ */
+
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *mover = (*stack)->prev;
+	int result = 0;
+	(void) line_number;
+
+	result = (mover->n % (*stack)->n);
+	mover->n = result;
+	_pop(stack, line_number);
+}
--- moves.c
+++ moves.c
@@ -1,4 +1,38 @@
 #include "monty.h"
 
-void _swap(stack_t **stack, unsigned int line_number);
-void _pop(stack_t **stack, unsigned int line_number);
+/**
+ * _pop - removes top node of stack
+ * @stack: pointer to top node
+ * @line_number: line number count for given file
+ */
+
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *mover;
+	(void) line_number;
+
+	if ((*stack)->prev == NULL)
+		free((*stack));
+	else
+	{
+		mover = (*stack);
+		(*stack) = (*stack)->prev;
+		free(mover);
+	}
+}
+
+/**
+ * _swap - a function to swap the top node and second node in a stack
+ * @stack: a pointer to the top of our stack
+ * @line_number: the line number of the command that called swap
+ */
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	int tmp = (*stack)->n;
+	stack_t *prev = (*stack)->prev;
+	(void) line_number;
+
+	(*stack)->n = prev->n;
+	prev->n = tmp;
+
+}
