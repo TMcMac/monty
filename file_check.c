b74cd35d573bc48b08729c00360fd5a7604855d8
Tim <timmcmackenjr@gmail.com>
2020-09-17 20:25:04 -0500

    betty fixes

--- file_check.c
+++ file_check.c
@@ -1,8 +1,9 @@
 #include "monty.h"
 
 /**
- * 
- * 
+ * file_check - a function to check we have rights and can open a filestream
+ * @fp: a pointer to a filestream
+ * @filename: the name of the file
  **/
 void file_check(FILE *fp, const char *filename)
 {
@@ -15,21 +16,23 @@ void file_check(FILE *fp, const char *filename)
 }
 
 /**
- * 
- * 
+ * clean_up - a function to free up any allocated memory
+ * @fp: a double pointer to our file stream
+ * @stack: a double pointer to our memory stack
+ * @buffer: adoubl pointer to our char buffer from getline
  **/
 void clean_up(FILE **fp, stack_t **stack, char **buffer)
 {
     stack_t *mover;
 
     if ((*fp) != NULL)
-      fclose((*fp));
+        fclose((*fp));
     if (*buffer != NULL)
         free(*buffer);
     while (*stack != NULL)
     {
         mover = (*stack);
-	(*stack) = (*stack)->prev;
+	    (*stack) = (*stack)->prev;
         free(mover);
     }
     free(info);
