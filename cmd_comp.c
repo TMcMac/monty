d4aac0ec1d4913aaf0a7f9e3d9c7a7c4760d0066
Tim <timmcmackenjr@gmail.com>
2020-09-17 20:30:47 -0500

    Betty Fixes

--- cmd_comp.c
+++ cmd_comp.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * opcode_comp - compare arg to avaiable op codes
+ * cmd_comp - compare arg to avaiable op codes
  * @line_num: line_number counter
  * @cmd: parsed getline for opcode and args
  * Return: Nothing
@@ -20,34 +20,34 @@ void cmd_comp(char *cmd[], stack_t **stack, unsigned int line_num)
 
 	for (i = 0; opcodes[i].opcode != NULL; i++)
 	{
-	    if (strcmp(opcodes[i].opcode, cmd[0]) == 0)
+		if (strcmp(opcodes[i].opcode, cmd[0]) == 0)
 		{
 			if (strcmp(opcodes[i].opcode, "push") == 0)
-			    {
-			        if (cmd[1] == NULL || isdigit(cmd[1][0]) == 0)
-				    {
-                        fprintf(stderr, "L%u: usage: push integer\n", line_num);
-				        clean_up(info->fp, stack, info->buffer);
-				        exit(EXIT_FAILURE);
-                    }
-			        if (isdigit(cmd[1][0]) != 0)
-			        {
-				        info->num = atoi(cmd[1]);
-				        opcodes[i].f(stack, line_num);
-				        return;
-			        }
-			    }
+			{
+				if (cmd[1] == NULL || isdigit(cmd[1][0]) == 0)
+				{
+					fprintf(stderr, "L%u: usage: push integer\n", line_num);
+					clean_up(info->fp, stack, info->buffer);
+					exit(EXIT_FAILURE);
+				}
+				if (isdigit(cmd[1][0]) != 0)
+				{
+					info->num = atoi(cmd[1]);
+					opcodes[i].f(stack, line_num);
+					return;
+				}
+			}
 			else
-            {
-			    opcodes[i].f(stack, line_num);
-                return;
-            }
+			{
+				opcodes[i].f(stack, line_num);
+				return;
+			}
 		}
 	}
 	if (opcodes[i].opcode == NULL)
 	{
-	    fprintf(stderr, "L%u: unknown instruction %s\n", line_num, cmd[0]);
-	    clean_up(info->fp, stack, info->buffer);
-	    exit(EXIT_FAILURE);
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_num, cmd[0]);
+		clean_up(info->fp, stack, info->buffer);
+		exit(EXIT_FAILURE);
 	}
 }
